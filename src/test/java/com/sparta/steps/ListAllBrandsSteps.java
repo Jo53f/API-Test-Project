65a2589ecede22053740a0e4db2ef202d205262d
Anthony Yanit <anyanit1@gmail.com>
2026-04-09 01:33:59 +0100

    Created Happy and Sad paths for listing all brands

--- src/test/java/com/sparta/steps/ListAllBrandsSteps.java
+++ src/test/java/com/sparta/steps/ListAllBrandsSteps.java
@@ -3,6 +3,7 @@ package com.sparta.steps;
 import com.sparta.pojobrands.Brands;
 import com.sparta.utils.ApiBuilder;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -45,5 +46,30 @@ public class ListAllBrandsSteps {
     @And("I should get a list of all the brands")
     public void iShouldGetAListOfAllTheBrands() {
         MatcherAssert.assertThat(brands.getBrands().size(), Matchers.is(34));
+    }
+
+    @Given("I send a put request for all brands")
+    public void iSendAPutRequestForAllBrands() {
+        response =
+                RestAssured
+                        .given()
+                        .accept(ContentType.JSON)
+                        .contentType(ContentType.JSON)
+                        .spec(ApiBuilder.putBrandsList())
+                        .when()
+                        .put()
+                        .then()
+                        .log().all()
+                        .extract().response();
+    }
+
+    @Then("the respons code should return 405")
+    public void theResponsCodeShouldReturn() {
+        MatcherAssert.assertThat(response.jsonPath().getString("responseCode"), Matchers.is("405"));
+    }
+
+    @And("I should get the response message This request method is not supported")
+    public void iShouldGetTheResponseMessageThisRequestMethodIsNotSupported() {
+        MatcherAssert.assertThat(response.jsonPath().getString("message"), Matchers.is("This request method is not supported."));
     }
 }
